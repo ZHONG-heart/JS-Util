a3b4e2fd2cdaf2e514f37322e5822f3bce0a2491
zhong <zhong_heart@163.com>
2020-02-19 16:47:13 +0800

    .

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,21 @@
 declare module "js-util" {
-  function formatDate(date: Date | number, formatter: string): string;
+  export function formatDate(date: Date | number, formatter: string): string;
   namespace formatDate {}
-  function getQueryParameter(name: string): string;
+  export function getQueryParameter(name: string): string;
   namespace getQueryParameter {}
-  function delParamsUrl(url: string, name: string): string;
+  export function delParamsUrl(url: string, name: string): string;
   namespace delParamsUrl {}
-  function selectText(element: any): void;
+  export function selectText(element: any): void;
   namespace selectText {}
-  function dataType(data: any): string;
+  export function dataType(data: any): string;
   namespace dataType {}
-  function ArrFlatSort(arr: Array<any>): Array<any>;
+  export function ArrFlatSort(arr: Array<any>): Array<any>;
   namespace ArrFlatSort {}
-  function destructuringArray(
+  export function destructuringArray(
     arr: Array<any>,
     str: string
   ): { [key: string]: string };
   namespace destructuringArray {}
-  function convert(str: string, mark: string): string;
+  export function convert(str: string, mark: string): string;
   namespace convert {}
 }
